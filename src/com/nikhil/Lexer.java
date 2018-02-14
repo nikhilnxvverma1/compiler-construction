fc1f9c71824a260c81bed6c0ce698941bbd9e1d5
Nikhil Verma <nikhilnxvverma1@aol.com>
2018-02-13 23:45:46 -0500

    Basic outline of elementary classes

---
+++ src/com/nikhil/Lexer.java
@@ -0,0 +1,9 @@
+package com.nikhil;
+
+/**
+ * Created by nikhilverma on 2/13/18.
+ */
+public class Lexer {
+    Token token;
+    String ch;
+}
---
+++ src/com/nikhil/Token.java
@@ -0,0 +1,21 @@
+package com.nikhil;
+
+/**
+ * Created by nikhilverma on 2/13/18.
+ */
+public enum Token {
+    EOF,
+    ID, //x
+    INT,
+    LPAREN,
+    RPAREN,
+    SCOLON,
+    WHILE,
+    AssignEQ,
+    CompareEQ,
+    MUL,
+    DIV,
+    PLUS,
+    LEQ,
+    IF,
+}
