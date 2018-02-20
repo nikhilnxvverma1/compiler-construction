742f1671cef662584eacb7777d281ba7de6b8d6a
Nikhil Verma <nikhilnxvverma1@aol.com>
2018-02-19 23:44:47 -0500

    Reference code for reading a file

--- src/com/nikhil/Main.java
+++ src/com/nikhil/Main.java
@@ -1,11 +1,16 @@
 package com.nikhil;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         String option;
-        String filename;
+        String filename="";
 
         if(args.length==0){
             System.out.println("Please give source file");
@@ -18,5 +23,38 @@ public class Main {
         }
 
         //TODO send the filename to respective operation class
+        //TODO the following code is only for reference purposes
+
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            // FileReader reads text files in the default encoding.
+            FileReader fileReader =
+                    new FileReader(filename);
+
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader =
+                    new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            // Always close files.
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                    "Unable to open file '" +
+                            filename + "'");
+        }
+        catch(IOException ex) {
+            System.out.println(
+                    "Error reading file '"
+                            + filename + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
     }
 }
