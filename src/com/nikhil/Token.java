8090869e8676ad399a0fa38046a23595f50c4077
Nikhil Verma <nikhilnxvverma1@aol.com>
2018-02-24 23:45:17 -0500

    Simple Token class for holding type and value(or id)

---
+++ src/com/nikhil/Token.java
@@ -0,0 +1,12 @@
+package com.nikhil;
+
+/**
+ * Created by nikhilverma on 2/24/18.
+ */
+public class Token {
+    TokenType tokenType;
+    /** In case of numeric values*/
+    int value;
+    /** In case of alphanumeric ids*/
+    int id;
+}
