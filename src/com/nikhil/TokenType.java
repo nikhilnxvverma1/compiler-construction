81c19c22170e5543ca775bb96760faff791f595f
Nikhil Verma <nikhilnxvverma1@aol.com>
2018-05-24 23:02:48 +0530

    Described token types against each enum

--- src/com/nikhil/TokenType.java
+++ src/com/nikhil/TokenType.java
@@ -5,30 +5,30 @@ package com.nikhil;
  * Created by nikhilverma on 2/13/18.
  */
 public enum TokenType {
-    EOF,
-    ID, //x
-    INT,
-    LPAREN,
-    RPAREN,
-    SEMICOLON,
-    WHILE,
-    ASSIGN_EQ,
-    COMPARE_EQ,
-    MUL,
-    DIV,
-    PLUS,
-    MINUS,
-    LESS_THAN_EQUAL_TO,
-    GREATER_THAN_EQUAL_TO,
-    LESS_THAN,
-    GREATER_THAN,
-    DOT,
-    IF,
-    ELSE,
-    INTERFACE,
-    VOID,
-    THIS,
-    NEW,
-    BOOLEAN,
-    SIDEF
+    EOF,// <end of file>
+    ID, // <x>
+    INT,// int
+    LPAREN,// {
+    RPAREN,// }
+    SEMICOLON,// ;
+    WHILE,// while
+    ASSIGN_EQ,// =
+    COMPARE_EQ,// ==
+    MUL,// *
+    DIV,// /
+    PLUS,//+
+    MINUS,// -
+    LESS_THAN_EQUAL_TO,// <=
+    GREATER_THAN_EQUAL_TO,// >=
+    LESS_THAN,// <
+    GREATER_THAN,// >
+    DOT,// .
+    IF,// if
+    ELSE,// else
+    INTERFACE,// interface
+    VOID,// void
+    THIS,// this
+    NEW,// new
+    BOOLEAN,// boolean
+    SIDEF// sidef
 }
